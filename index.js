9cb88c569abda777acd35de520a13b6491cb5a6f
itsluispatricio <dev@luispatricio.com>
2023-07-31 01:19:38 -0500

    change

--- index.js
+++ index.js
@@ -1,90 +1,95 @@
-var axios = require('axios');
-var express = require('express');
+var axios = require("axios");
+var express = require("express");
 var app = express();
 
 // Middleware to parse incoming request bodies
 app.use(express.urlencoded({ extended: true }));
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+// Serve static assets from the 'public' folder
+app.use(express.static("public"));
 
 // use res.render to load up an ejs view file
 
 // index page
-app.get('/', function(req, res) {
+app.get("/", function (req, res) {
   if (req.query.setID) {
     res.render(path.join(__dirname, "pages/index"));
   } else {
     res.render(path.join(__dirname, "pages/index"), {
-      id: req.query.preset
+      id: req.query.preset,
     });
   }
 });
 
 // create recipe function
-app.post('/createRecipe', function(req, res) {
+app.post("/createRecipe", function (req, res) {
   const createRecipeOptions = {
-    method: 'POST',
-    url: 'https://api.recipesai.co/createRecipe',
-    headers: { 'content-type': 'application/json' },
-    data: { food: req.body.food }
+    method: "POST",
+    url: "https://api.recipesai.co/createRecipe",
+    headers: { "content-type": "application/json" },
+    data: { food: req.body.food },
   };
 
   axios
     .request(createRecipeOptions)
-    .then(function(response) {
+    .then(function (response) {
       const recipeID = response.data.recipeID;
       // Call the completeRecipeOptions asynchronously using async/await
       completeRecipe(recipeID)
         .then(() => {
           res.redirect(`/waiting/${recipeID}`);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
-          res.status(500).send('An error occurred while processing the recipe.');
+          res.status(500).send("An error occurred while processing the recipe.");
         });
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.error(error);
-      res.status(500).send('An error occurred while creating the recipe.');
+      res.status(500).send("An error occurred while creating the recipe.");
     });
 });
 
 // Function to call completeRecipeOptions asynchronously
 async function completeRecipe(recipeID) {
   const completeRecipeOptions = {
-    method: 'POST',
-    url: 'https://api.recipesai.co/completeRecipe',
-    headers: { 'content-type': 'application/json' },
-    data: { id: recipeID }
+    method: "POST",
+    url: "https://api.recipesai.co/completeRecipe",
+    headers: { "content-type": "application/json" },
+    data: { id: recipeID },
   };
 
   await axios.request(completeRecipeOptions);
 }
 
 // waiting page
-app.use('/waiting', function(req, res) {
-
+app.use("/waiting", function (req, res) {
   const options = {
-    method: 'GET',
+    method: "GET",
     url: `https://backend.nex.sh/items/recipes${req.path}`,
-    headers: {Authorization: 'Bearer NHZnU4dOtSE4SXgNHonq1z1u-75jhI7c'}
+    headers: { Authorization: "Bearer NHZnU4dOtSE4SXgNHonq1z1u-75jhI7c" },
   };
-  
-  axios.request(options).then(function (response) {
-    if (response.data.data.status == "draft") {
-      res.render(path.join(__dirname, "pages/process"), {
-        processDetail: 'Generating your recipe using AI...'
-      });
-    } else {
-      res.render(path.join(__dirname, "pages/iFrameRedirect"), {
-        recipeID: req.path
-      });
-    }
-  }).catch(function (error) {
-    console.error(error);
-  });
+
+  axios
+    .request(options)
+    .then(function (response) {
+      if (response.data.data.status == "draft") {
+        res.render(path.join(__dirname, "pages/process"), {
+          processDetail: "Generating your recipe using AI...",
+        });
+      } else {
+        res.render(path.join(__dirname, "pages/iFrameRedirect"), {
+          recipeID: req.path,
+        });
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 });
 
 app.listen(8080);
-console.log('Server is listening on port 8080');
+console.log("Server is listening on port 8080");
